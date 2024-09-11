02c472c3ab4c998e6a4fb3d0955fca8aadc961ba
daichi2mori <daichi2mori@gmail.com>
2024-09-11 14:26:53 +0900

    feat: if auth/google is accessed directly, the redirect destination is changed according to the login status

--- app/routes/auth.google.tsx
+++ app/routes/auth.google.tsx
@@ -1,11 +1,21 @@
-import { type ActionFunctionArgs, redirect } from "@remix-run/cloudflare";
+import {
+	type ActionFunctionArgs,
+	type LoaderFunctionArgs,
+	redirect,
+} from "@remix-run/cloudflare";
 import { getAuthenticator } from "~/services/auth.server";
 
-export const loader = () => redirect("/login");
-
-export const action = ({ request, context }: ActionFunctionArgs) => {
+export const loader = async ({ request, context }: LoaderFunctionArgs) => {
 	const authenticator = getAuthenticator(context);
-	return authenticator.authenticate("google", request, {
+	const user = await authenticator.isAuthenticated(request);
+	if (!user) return redirect("/login");
+
+	return redirect(`/user/${user.profileId}`);
+};
+
+export const action = async ({ request, context }: ActionFunctionArgs) => {
+	const authenticator = getAuthenticator(context);
+	return await authenticator.authenticate("google", request, {
 		successRedirect: "/user",
 		failureRedirect: "/login",
 	});
