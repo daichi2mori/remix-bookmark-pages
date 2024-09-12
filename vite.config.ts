dde6e4d8f904aa819d401633b893a5dc8d1577f7
daichi2mori <daichi2mori@gmail.com>
2024-09-12 23:36:51 +0900

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
+import { remixPWA } from "@remix-pwa/dev";
 import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    remixCloudflareDevProxy(),
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+	plugins: [
+		remixCloudflareDevProxy(),
+		remix({
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+			},
+		}),
+		remixPWA(),
+		tsconfigPaths(),
+	],
 });
